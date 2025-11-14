8a85d23fbd6deef59b13455ef9b76d15951b7ed6
dhivya1222 <dhivyanandhagopal12@gmail.com>
2025-11-14 14:25:56 +0530

    Change API invoke URL in config.js
    Updated the API invoke URL in the configuration.

--- js/config.js
+++ js/config.js
@@ -5,7 +5,8 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://vpdv0dwyeh.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://cigm5pn0m2.execute-api.ap-south-1.amazonaws.com/ryder' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
