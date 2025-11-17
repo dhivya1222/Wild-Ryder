a90957db4624b553ecc0a0704000824119d7c845
dhivya1222 <dhivyanandhagopal12@gmail.com>
2025-11-17 11:02:07 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_egMBhajyy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5pjp6jhibbcb6fu9trrdj0sbeh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_WsjU8RGeD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '44mbdpsink35iaa5rmn9kjeupp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://cigm5pn0m2.execute-api.ap-south-1.amazonaws.com/ryder' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://2ajvetwxwa.execute-api.ap-south-1.amazonaws.com/wild',
     }
 };
+
 
 
